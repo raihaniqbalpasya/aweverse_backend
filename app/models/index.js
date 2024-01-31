299ba73c1332a6f29cb3fb6b10843ae8b7db81d4
unknown <19104030@ittelkom-pwt.ac.id>
2024-01-31 11:48:33 +0700

    fix env error

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,4 @@
 "use strict";
-require("dotenv").config();
 
 require("pg");
 require("@vercel/postgres");
@@ -8,7 +7,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../../config/database.js")[env];
 const db = {};
 
